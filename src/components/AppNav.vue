377fe1893de4b15964aee24d53b3e2d3fb241eb4
Duckhee Lee <shldhee@yahoo.co.kr>
2020-11-01 21:01:05 +0900

    feat: AppNav 컴포넌트 생성

---
+++ src/components/AppNav.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex justify-between">
+    <router-link to="/">블로그</router-link>
+    <template v-if="loggined">
+      <button>로그인</button>
+    </template>
+    <template v-else>
+      <button>로그아웃</button>
+    </template>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    const loggined = ref(true);
+    return { loggined };
+  }
+});
+</script>
+
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,18 +3,20 @@
     <div class="m-10">
       Tailwind
     </div>
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { defineComponent } from "vue";
+export default defineComponent({});
+// import { Options, Vue } from "vue-class-component";
+// import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
-@Options({
-  components: {
-    HelloWorld
-  }
-})
-export default class Home extends Vue {}
+// @Options({
+//   components: {
+//     HelloWorld
+//   }
+// })
+// export default class Home extends Vue {}
 </script>
